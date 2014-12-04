2a6185e6298b9cc52e93c54b2decf76c788d864e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-04 14:33:28 -0500

    checking if there is error message in stderr, fixes #1

--- tasks/help.js
+++ tasks/help.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
         done(false);
         return;
       }
-      if (stderr) {
+      if (/error/i.test(stderr)) {
         grunt.log.warn(stderr);
         done(false);
         return;
@@ -50,29 +50,6 @@ module.exports = function(grunt) {
       grunt.log.writeln(stdout);
       done(true);
     });
-
-    /*
-    (function runFixPack() {
-      var join = require('path').join;
-      var fixpack = join(__dirname, '../node_modules/fixpack');
-      var exec = require('child_process').exec;
-
-      exec('node ' + fixpack, function (error, stdout, stderr) {
-        if (error) {
-          grunt.log.error(error);
-          done(false);
-        } else {
-          grunt.log.writeln(stdout);
-          if (stderr) {
-            grunt.log.warn(stderr);
-          }
-          done(!!result.valid);
-        }
-      });
-
-    }());
-    */
-
   });
 
 };
